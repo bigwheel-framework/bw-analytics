4d4f3a89e479b7918a3ceb2aafd1555944c56d7d
Nick <nick@jam3.com>
2015-08-24 13:24:11 -0400

    :arrow_down: :arrow_lower_right: :arrow_right: :b: :boom:

---
+++ README.md
@@ -0,0 +1,13 @@
+# bw-analytics
+
+[![experimental](http://badges.github.io/stability-badges/dist/experimental.svg)](http://github.com/badges/stability-badges)
+
+bigwheel google analytics plugin
+
+## Usage
+
+[![NPM](https://nodei.co/npm/bw-analytics.png)](https://www.npmjs.com/package/bw-analytics)
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/njam3/bw-analytics/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = function(ua,domain,bw) {
+  if (window && typeof ua === 'string') {
+    if (typeof domain !== 'string') {
+      router = domain;
+      domain = undefined;
+    }
+  	if (!window.ga) {
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    }
+    ga('create',ua,domain || 'auto');
+    ga('send','pageview');
+    bw.on('route',function(data) {
+      var page = window.location.hash.replace('#!','');
+      if (!page) page = '/';
+      ga('send', 'pageview', page);
+    });
+  } else {
+    console.log('This version of GA is only available in the browser.');
+  }
+};
